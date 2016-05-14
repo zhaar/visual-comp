153ebbaa61999951bc032077fdffacef98784d50
zephyz <zephyz.zhaar@gmail.com>
2016-05-14 17:04:03 +0200

    image transformation is a subtype of function

--- Image Processing/src/ch/epfl/visualComputing/ImageTransformation.java
+++ Image Processing/src/ch/epfl/visualComputing/ImageTransformation.java
@@ -1,23 +1,15 @@
 package ch.epfl.visualComputing;
 
 import ch.epfl.visualComputing.Transformations.Convolution;
+import ch.epfl.visualComputing.Transformations.TransformationFunction;
 
 import java.util.List;
-import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
-import java.util.function.BinaryOperator;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class ImageTransformation<Pixel, Transformed> {
-
-    @FunctionalInterface
-    public interface PixelTransformation<T, S> {
-        S apply(T pixel);
-        default GenericTransformation<T, S> toGeneric() {
-            return (index, pixel, image) -> PixelTransformation.this.apply(pixel);
-        }
-    }
+public class ImageTransformation<Pixel, Transformed> implements TransformationFunction<List<Pixel>, List<Transformed>>{
 
     @FunctionalInterface
     public interface GenericTransformation<T, S> {
@@ -26,28 +18,16 @@ public class ImageTransformation<Pixel, Transformed> {
 
     private final GenericTransformation<Pixel, Transformed> transform;
 
-    public ImageTransformation(PixelTransformation<Pixel, Transformed> px) {
-        this.transform = px.toGeneric();
-    }
-
     public ImageTransformation(GenericTransformation<Pixel, Transformed> trs) {
         this.transform = trs;
     }
-
 
     public static ImageTransformation<Float, Float> convolutionTransformation(int[][] matrix, int size, int w, int h) {
         return new ImageTransformation<>(new Convolution(matrix, size, 1).toGeneric(w, h));
     }
 
-//    public <T> ImageTransformation<Pixel, T>composeWith(ImageTransformation<Transformed, T> ts) {
-//        return new ImageTransformation<Pixel, T>((i, p, ls) -> );
-//    }
-
-    public <T> ImageTransformation<Pixel, T> mergeTransforms(ImageTransformation<Pixel, Transformed> that, BiFunction<Transformed, Transformed, T> merger) {
-        return new ImageTransformation<Pixel, T>((i, p, ls) -> merger.apply(this.transform.apply(i, p, ls), that.transform.apply(i, p, ls)));
-    }
-
-    public List<Transformed> transform(List<Pixel> source) {
+    @Override
+    public List<Transformed> apply(List<Pixel> source) {
         return IntStream.range(0, source.size()).parallel()
                 .mapToObj((i) -> transform.apply(i, source.get(i), source))
                 .collect(Collectors.toList());
