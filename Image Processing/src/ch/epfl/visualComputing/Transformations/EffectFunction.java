418450609c67787290863cf92835e1022a7117ca
zephyz <zephyz.zhaar@gmail.com>
2016-05-14 17:07:53 +0200

    add fonctionalities to function class

---
+++ Image Processing/src/ch/epfl/visualComputing/Transformations/EffectFunction.java
@@ -0,0 +1,18 @@
+package ch.epfl.visualComputing.Transformations;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class EffectFunction<A> implements Function<A, A> {
+
+    private final Consumer<A> effect;
+
+    public EffectFunction(Consumer<A> effect) {
+        this.effect = effect;
+    }
+    @Override
+    public A apply(A a) {
+        effect.accept(a);
+        return a;
+    }
+}
---
+++ Image Processing/src/ch/epfl/visualComputing/Transformations/TransformationFunction.java
@@ -0,0 +1,23 @@
+package ch.epfl.visualComputing.Transformations;
+
+
+import ch.epfl.visualComputing.CopeOut.Pair;
+
+import java.util.Objects;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+@FunctionalInterface
+public interface TransformationFunction<A, B> extends Function<A, B> {
+
+    default <C> Function<A, Pair<B, C>> merge(Function<A, C> that) {
+        Objects.requireNonNull(that);
+        return (A a) -> new Pair<>(this.apply(a), that.apply(a));
+    }
+
+    default <C, D> Function<A, D> mergeWith(Function<A, C> that, BiFunction<B, C, D> fn) {
+        Objects.requireNonNull(that);
+        Objects.requireNonNull(fn);
+        return merge(that).andThen((Pair<B, C> p) -> fn.apply(p._1(), p._2()));
+    }
+}
