9f5b75ae0b016b9df17dd0fc8bdfe34d9c708cf1
zephyz <zephyz.zhaar@gmail.com>
2016-05-15 19:45:09 +0200

    implement webcam

--- Image Processing/src/ch/epfl/visualComputing/ImageProcessing.java
+++ Image Processing/src/ch/epfl/visualComputing/ImageProcessing.java
@@ -1,13 +1,13 @@
 package ch.epfl.visualComputing;
 
 import ch.epfl.visualComputing.CopeOut.DepressingJava;
-import ch.epfl.visualComputing.CopeOut.Triple;
 import ch.epfl.visualComputing.Transformations.Convolution;
 import ch.epfl.visualComputing.Transformations.Effects.DrawEffects;
 import ch.epfl.visualComputing.Transformations.HoughTransformation;
 import ch.epfl.visualComputing.Transformations.PixelTransformer;
 import processing.core.PApplet;
 import processing.core.PImage;
+import processing.video.Capture;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,6 +16,9 @@ import java.util.stream.Collectors;
 public class ImageProcessing extends PApplet {
 
     PImage img;
+    Capture cam;
+
+    private static final boolean webcam = true;
 
     HScrollbar upperThreshold = new HScrollbar(this, 0, 0, 800, 12);
     HScrollbar lowerThreshold = new HScrollbar(this, 0, 15, 800, 12);
@@ -25,14 +28,37 @@ public class ImageProcessing extends PApplet {
     }
 
     public void setup() {
-        img = loadImage("board1.jpg");
-        noLoop();
+        if (webcam) {
+            String[] cameras = Capture.list();
+            if (cameras.length == 0) {
+                println("There are no cameras available for capture.");
+                exit();
+            } else {
+                println("Available cameras:");
+                for (String camera : cameras) {
+                    println(camera);
+                }
+                cam = new Capture(this, cameras[0]);
+                cam.start();
+            }
+        } else {
+            img = loadImage("board1.jpg");
+            noLoop();
+        }
     }
 
     public void draw() {
-        computeImage(img);
-        upperThreshold.update();
-        upperThreshold.display();
+        if (webcam) {
+            if (cam.available()) {
+                cam.read();
+            }
+            img = cam.get();
+            image(img, 0, 0);
+        } else {
+            computeImage(img);
+            upperThreshold.update();
+            upperThreshold.display();
+        }
     }
 
     public void computeImage(PImage img) {
